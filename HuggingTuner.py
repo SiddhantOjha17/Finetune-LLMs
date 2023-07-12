88b015460b0a462124d238bdbe797dea7612ebb9
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-12 19:53:02 +0530

    Add autotrain

--- HuggingTuner.py
+++ HuggingTuner.py
@@ -1,35 +1,53 @@
-from transformers import (
-    AutoTokenizer,
-    AutoModelForCausalLM,
-    BitsAndBytesConfig,
-    Trainer,
-    DataCollatorForLanguageModeling,
-    TrainingArguments,
-)
 import torch
-from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
 from datasets import load_dataset
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from trl import SFTTrainer
 
 
-class Tuner:
-    def __init__(self, model_id) -> None:
-        self.model_id = self.model_id
+def train():
+    train_dataset = load_dataset("tatsu-lab/alpaca", split=True)
+    tokenizer = AutoTokenizer.from_pretrained(
+        "Salesforce/xgen-7b-8k-base", trust_remote_code=True
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    model = AutoModelForCausalLM.from_pretrained(
+        "Salesforce/xgen-7b-8k-base",
+        load_in_4bit=True,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+    model.resize_token_embeddings(len(tokenizer))
+    model = prepare_model_for_int8_training(model)
+    peft_config = LoraConfig(
+        r=16, lora_alpha=32, lora_dropout=0.05, bias="none", task_type="CAUSAL_LM"
+    )
+    model = get_peft_model(model, peft_config)
+    training_args = TrainingArguments(
+        output_dir="xgen-7b-tuned-alpaca",
+        per_device_train_batch_size=4,
+        optim="adamw_torch",
+        logging_steps=100,
+        learning_rate=2e-4,
+        fp16=True,
+        warmup_ratio=0.1,
+        lr_schedular_type="linear",
+        num_train_epochs=1,
+        save_strategy="epoch",
+        push_to_hub=True,
+    )
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_dataset,
+        dataset_test_field="text",
+        max_seq_length=1024,
+        args=training_args,
+        packing=True,
+        peft_config=peft_config,
+    )
+    trainer.train()
+    trainer.push_to_hub()
 
-    def _setup_bnb_config(self):
-        self.bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16,
-        )
-        return None
 
-    def _setup_model_and_tokenizer(self):
-        tokenizer = AutoTokenizer.from_pretrained(self.model_id)
-
-        model = AutoModelForCausalLM.from_pretrained(
-            self.model_id,
-            quantization_config=self._setup_bnb_config(),
-            device_map={"": 0},
-        ).gradient_checkpointing_enable()
-        model = prepare_model_for_kbit_training(model)
+if __name__ == "__main__":
+    train()
